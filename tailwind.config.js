9712c719ef3544397d2426f8bc947d530f6c5199
Quang Hung <hungpham307@gmail.com>
2023-03-08 20:19:15 +0700

    style: replace typeface

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        flow: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(calc(-100% - 2rem))' },
+        }
+      },
+      animation: {
+        flow: 'flow 22s linear infinite'
+      },
+      fontFamily: {
+        sans: ['Manrope', 'sans-serif'],
+        serif: ['Playfair Display', 'serif'],
+      }
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 }
