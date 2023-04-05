01e5e433925db2ce49a653935f7fa9f86f186bdd
Quang Hung <hungpham307@gmail.com>
2023-04-05 11:56:50 +0700

    fix: appropriate loader to handle mdx file type (remarkGfm)

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+import nextMDX from '@next/mdx'
+import remarkGfm from 'remark-gfm'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+}
+
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [],
+  },
+})
+
+export default withMDX(nextConfig)
