f7b296e9e3b5410222e1f46d3ef99559bbf1120e
Quang Hung <hungpham307@gmail.com>
2023-04-07 21:02:27 +0700

    feat: build the layout for the article

---
+++ src/components/ArticleLayout.tsx
@@ -0,0 +1,35 @@
+import Head from "next/head";
+import { formatDate } from "@/utils/formatDate";
+
+interface ArticleMetadata {
+  title: string;
+  description: string;
+  date: string;
+  slug: string;
+}
+
+export function ArticleLayout({
+  metadata,
+  children,
+}: {
+  metadata: ArticleMetadata;
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </Head>
+      <div className="mx-auto mt-16 max-w-2xl sm:mt-20">
+        <header>
+          <div className="flex items-center text-base text-zinc-400">
+            <span className="h-4 w-0.5 rounded-full bg-zinc-200" />
+            <span className="ml-2">{formatDate(metadata.date)}</span>
+          </div>
+        </header>
+        <article className="prose mt-8">{children}</article>
+      </div>
+    </>
+  );
+}
