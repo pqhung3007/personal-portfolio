3a6f98e8022801947148333cb7c6751c715e3e4a
Quang Hung <hungpham307@gmail.com>
2023-04-12 10:06:31 +0700

    feat: add navigate-to-top button

--- src/components/ArticleLayout.tsx
+++ src/components/ArticleLayout.tsx
@@ -1,5 +1,8 @@
 import Head from "next/head";
+import { useState, useEffect } from "react";
+
 import { formatDate } from "@/utils/formatDate";
+import { ArrowUpIcon } from "@heroicons/react/24/outline";
 
 interface ArticleMetadata {
   title: string;
@@ -15,13 +18,30 @@ export function ArticleLayout({
   metadata: ArticleMetadata;
   children: React.ReactNode;
 }) {
+  const [isVisible, setIsVisible] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", showScrollButton);
+    return () => window.removeEventListener("scroll", showScrollButton);
+  }, []);
+
+  function showScrollButton() {
+    return window.scrollY > 500 ? setIsVisible(true) : setIsVisible(false);
+  }
+
+  function handleScrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "auto",
+    });
+  }
+
   return (
     <>
       <Head>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
       </Head>
-      <div className="mx-auto mt-16 max-w-2xl sm:mt-20">
+      <div className="relative mx-auto mt-16 max-w-2xl px-4 sm:mt-20 lg:px-0">
         <header>
           <div className="flex items-center text-base text-zinc-400">
             <span className="h-4 w-0.5 rounded-full bg-zinc-200" />
@@ -29,6 +49,16 @@ export function ArticleLayout({
           </div>
         </header>
         <article className="prose mt-8 dark:prose-invert">{children}</article>
+
+        <button
+          type="button"
+          className={`${
+            isVisible ? "block" : "hidden"
+          } group fixed bottom-12 right-20 rounded-full bg-white p-3 shadow-md shadow-zinc-800/5 dark:bg-zinc-700`}
+          onClick={handleScrollToTop}
+        >
+          <ArrowUpIcon className="h-6 w-6 text-zinc-400 transition group-hover:text-zinc-600 dark:group-hover:text-zinc-200" />
+        </button>
       </div>
     </>
   );
