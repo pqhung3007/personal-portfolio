c10b54059bc4b89f8c439eac5bbe577e999091f0
Quang Hung <hungpham307@gmail.com>
2023-04-13 21:49:46 +0700

    amend: extract interfaces to a single file

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -4,3 +4,16 @@ export interface ArticleMetadata {
   date: string;
   slug: string;
 }
+
+export interface SocialLinkProps {
+  href: string;
+  icon: React.FC<React.SVGProps<SVGSVGElement>>;
+  className?: string;
+  children?: React.ReactNode;
+}
+
+export interface LayoutProps {
+  title: string;
+  intro: string;
+  children: React.ReactNode;
+}
